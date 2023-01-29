6ebbe1ad1a3e9283b14f06e5758dfa8ea55894fc
AkashHiwale <akashhiwale452@gmail.com>
2023-01-29 15:30:02 +0530

    Methods to Iteration class added

--- Basics/Iteration/Iteration.cs
+++ Basics/Iteration/Iteration.cs
@@ -32,4 +32,41 @@ public class Iteration
         }
         return output;
     }
+
+    public static int DivisibleRangeCount(int n)
+    {
+        int count = 0;
+        for(var i=1; i<=100; i++)
+        {
+            if(i%n == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int Factorial(int n)
+    {
+        int factorial = 1;
+        while(n>0)
+        {
+            factorial = factorial * n;
+            n--;
+        }
+        return factorial;
+    }
+
+    public static int FindMaximum(string commaSeparatedNumbers)
+    {
+        var list = commaSeparatedNumbers.Split(",").ToList();
+        var listOfNumbers = new List<int>();
+
+        foreach(var num in list)
+        {
+            listOfNumbers.Add((int)Convert.ToInt64(num));
+        }
+        var maxNumInList = listOfNumbers.Max();
+        return maxNumInList;
+    }
 }
